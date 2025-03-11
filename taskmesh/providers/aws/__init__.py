28905df1e4f0df86f2cc51c55a8c6c7ad4acbf3c
Amrit K. Chhetri <kshetriamrit@gmail.com>
2025-03-10 23:02:54 -0500

    restructured project

--- taskmesh/providers/aws/__init__.py
+++ taskmesh/providers/aws/__init__.py
@@ -1,6 +1,1 @@
 from .provider import AWSProvider
-
-
-class Provider:
-    def run(self, topic, func, config):
-        raise NotImplementedError("This method should be implemented by the subclass")
